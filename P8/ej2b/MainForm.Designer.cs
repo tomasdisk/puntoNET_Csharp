d945b84ff962351072f476a1547b7bd8dbfb8725
tomasdisk <tomasdisk@hotmail.com>
2017-12-11 13:31:09 -0300

    practica 8 done

---
+++ P8/ej2b/MainForm.Designer.cs
@@ -0,0 +1,312 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tomm
+ * Date: 11/12/2017
+ * Time: 11:22 a.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ej2b
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.panel1.SuspendLayout();
+			this.groupBox4.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.groupBox2.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Controls.Add(this.groupBox4);
+			this.panel1.Controls.Add(this.groupBox3);
+			this.panel1.Controls.Add(this.groupBox2);
+			this.panel1.Controls.Add(this.groupBox1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(371, 467);
+			this.panel1.TabIndex = 0;
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Controls.Add(this.label3);
+			this.groupBox4.Controls.Add(this.label2);
+			this.groupBox4.Location = new System.Drawing.Point(201, 282);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(158, 118);
+			this.groupBox4.TabIndex = 4;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "Información";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(6, 71);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(140, 20);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "Width=000, Height=000";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(6, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(140, 18);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Left=000, Top=000";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.numericUpDown1);
+			this.groupBox3.Location = new System.Drawing.Point(201, 196);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(158, 80);
+			this.groupBox3.TabIndex = 3;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Tamaño de la letra";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(26, 37);
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+			8,
+			0,
+			0,
+			0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(109, 20);
+			this.numericUpDown1.TabIndex = 0;
+			this.numericUpDown1.Tag = "";
+			this.numericUpDown1.Value = new decimal(new int[] {
+			12,
+			0,
+			0,
+			0});
+			this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1ValueChanged);
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Location = new System.Drawing.Point(12, 406);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(347, 49);
+			this.groupBox2.TabIndex = 2;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Tiempo";
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.Color.Blue;
+			this.label4.Location = new System.Drawing.Point(123, 16);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(107, 30);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "00:00:00";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Controls.Add(this.button4);
+			this.groupBox1.Controls.Add(this.button3);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.checkBox1);
+			this.groupBox1.Controls.Add(this.comboBox1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 196);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(183, 204);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Controles de movimiento";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(31, 157);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(121, 16);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Paso del movimiento";
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(123, 100);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(49, 45);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "der";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(13, 100);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(49, 45);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "izq";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(68, 100);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(49, 45);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "abajo";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(68, 49);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(49, 45);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "arriba";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(31, 19);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(104, 24);
+			this.checkBox1.TabIndex = 1;
+			this.checkBox1.Text = "Limitar al Panel";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckStateChanged += new System.EventHandler(this.CheckBox1CheckStateChanged);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(31, 177);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.White;
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Location = new System.Drawing.Point(12, 12);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(347, 178);
+			this.panel2.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Lime;
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(123, 65);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 22);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Palabra";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(371, 467);
+			this.Controls.Add(this.panel1);
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(387, 506);
+			this.MinimumSize = new System.Drawing.Size(387, 506);
+			this.Name = "MainForm";
+			this.Text = "Ejercicio2";
+			this.panel1.ResumeLayout(false);
+			this.groupBox4.ResumeLayout(false);
+			this.groupBox3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox1.ResumeLayout(false);
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ P8/ej2b/MainForm.cs
@@ -0,0 +1,136 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: tomm
+ * Date: 11/12/2017
+ * Time: 11:22 a.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace ej2b
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		int h=0,m=0,s=0;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			label4.Text = String.Format("{0:00}:{1:00}:{2:00}",h,m,s);
+			comboBox1.Items.AddRange(Enumerable.Range(1, 100).Select(i => (object) i).ToArray());
+			comboBox1.SelectedIndex = 9;
+			label1.Top = panel2.Height / 2 - label1.Height / 2;
+			label1.Left = panel2.Width / 2 - label1.Width / 2;
+			draw("info");
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			s++;
+			if(s == 60){
+				s = 0;
+				m++;
+				if(m == 60){
+					m = 0;
+					h++;
+					if(h == 99){
+						h = 0;
+					}
+				}
+			}
+			label4.Text = String.Format("{0:00}:{1:00}:{2:00}",h,m,s);
+		}
+		void CheckBox1CheckStateChanged(object sender, EventArgs e)
+		{
+			draw("check");
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			draw("up");
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+			draw("left");
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			draw("down");
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+			draw("rigth");
+		}
+		void Label1Click(object sender, EventArgs e)
+		{
+			//color
+			
+		}
+		void NumericUpDown1ValueChanged(object sender, EventArgs e)
+		{
+			draw("size");
+		}
+				
+		void draw(string op)
+		{
+			//redibujar con propiedades actuales
+			if (op == "up")
+			{
+				label1.Top -= (int)comboBox1.SelectedItem;
+				label1.Text = "Arriba";
+			}
+			if (op == "left")
+			{
+				label1.Left -= (int)comboBox1.SelectedItem;
+				label1.Text = "Izquierda";
+			}
+			if (op == "down")
+			{
+				label1.Top += (int)comboBox1.SelectedItem;
+				label1.Text = "Abajo";
+			}
+			if (op == "rigth")
+			{
+				label1.Left += (int)comboBox1.SelectedItem;
+				label1.Text = "Derecha";
+			}
+			if (op == "size")
+			{
+				label1.Font = new Font(label1.Font.FontFamily, (float)numericUpDown1.Value);;
+			}
+			if (checkBox1.Checked)
+			{
+				if((label1.Left + label1.Width) >= panel2.Width){
+					label1.Left = panel2.Width - label1.Width;
+				}
+				if((label1.Top + label1.Height) >= panel2.Height){
+					label1.Top = panel2.Height - label1.Height;
+				}
+				if(label1.Top <= 0){
+					label1.Top = 0;
+				}
+				if(label1.Left <= 0){
+					label1.Left = 0;
+
+				}
+			}
+			
+			//cambiar informacion
+			label2.Text = "Left="+ label1.Left +", Top="+ label1.Top;
+			label3.Text = "Width="+ label1.Width +", Height="+ label1.Height;
+		}
+	}
+}
