429a67b44202e9351aeb1af675351abe33ddfa65
tomasdisk <tomasdisk@hotmail.com>
2017-12-11 21:37:47 -0300

    preactica 9 done

---
+++ P9/ej1/MainForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Carlos
+ * Date: 11/12/2017
+ * Time: 04:43 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ej1
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem abrirCarpetaToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem visualizaciónToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem ajustarALaVentanaToolStripMenuItem;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripMenuItem aToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+		private System.Windows.Forms.ToolStripMenuItem presentaciónToolStripMenuItem;
+		private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Timer timer1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.abrirCarpetaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.visualizaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ajustarALaVentanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+			this.presentaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.statusStrip1.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.splitContainer1);
+			this.panel1.Controls.Add(this.statusStrip1);
+			this.panel1.Controls.Add(this.menuStrip1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(522, 279);
+			this.panel1.TabIndex = 0;
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+			this.splitContainer1.Size = new System.Drawing.Size(522, 233);
+			this.splitContainer1.SplitterDistance = 174;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// listBox1
+			// 
+			this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(0, 0);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(174, 233);
+			this.listBox1.TabIndex = 0;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1SelectedIndexChanged);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(344, 233);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 257);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(522, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+			this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.archivoToolStripMenuItem,
+			this.visualizaciónToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(522, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// archivoToolStripMenuItem
+			// 
+			this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.abrirCarpetaToolStripMenuItem,
+			this.toolStripSeparator1,
+			this.salirToolStripMenuItem});
+			this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+			this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+			this.archivoToolStripMenuItem.Text = "Archivo";
+			// 
+			// abrirCarpetaToolStripMenuItem
+			// 
+			this.abrirCarpetaToolStripMenuItem.Name = "abrirCarpetaToolStripMenuItem";
+			this.abrirCarpetaToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+			this.abrirCarpetaToolStripMenuItem.Text = "Abrir Carpeta";
+			this.abrirCarpetaToolStripMenuItem.Click += new System.EventHandler(this.AbrirCarpetaToolStripMenuItemClick);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(141, 6);
+			// 
+			// salirToolStripMenuItem
+			// 
+			this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+			this.salirToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+			this.salirToolStripMenuItem.Text = "Salir";
+			this.salirToolStripMenuItem.Click += new System.EventHandler(this.SalirToolStripMenuItemClick);
+			// 
+			// visualizaciónToolStripMenuItem
+			// 
+			this.visualizaciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.ajustarALaVentanaToolStripMenuItem,
+			this.toolStripSeparator2,
+			this.aToolStripMenuItem,
+			this.toolStripSeparator3,
+			this.presentaciónToolStripMenuItem,
+			this.toolStripComboBox1});
+			this.visualizaciónToolStripMenuItem.Name = "visualizaciónToolStripMenuItem";
+			this.visualizaciónToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+			this.visualizaciónToolStripMenuItem.Text = "Visualización";
+			// 
+			// ajustarALaVentanaToolStripMenuItem
+			// 
+			this.ajustarALaVentanaToolStripMenuItem.Name = "ajustarALaVentanaToolStripMenuItem";
+			this.ajustarALaVentanaToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+			this.ajustarALaVentanaToolStripMenuItem.Text = "Ajustar a la Ventana";
+			this.ajustarALaVentanaToolStripMenuItem.Click += new System.EventHandler(this.AjustarALaVentanaToolStripMenuItemClick);
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(254, 6);
+			// 
+			// aToolStripMenuItem
+			// 
+			this.aToolStripMenuItem.Checked = true;
+			this.aToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.aToolStripMenuItem.Name = "aToolStripMenuItem";
+			this.aToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+			this.aToolStripMenuItem.Text = "Ver Lista de Imágenes";
+			this.aToolStripMenuItem.Click += new System.EventHandler(this.AToolStripMenuItemClick);
+			// 
+			// toolStripSeparator3
+			// 
+			this.toolStripSeparator3.Name = "toolStripSeparator3";
+			this.toolStripSeparator3.Size = new System.Drawing.Size(254, 6);
+			// 
+			// presentaciónToolStripMenuItem
+			// 
+			this.presentaciónToolStripMenuItem.Name = "presentaciónToolStripMenuItem";
+			this.presentaciónToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+			this.presentaciónToolStripMenuItem.Text = "Presentación";
+			this.presentaciónToolStripMenuItem.Click += new System.EventHandler(this.PresentaciónToolStripMenuItemClick);
+			// 
+			// toolStripComboBox1
+			// 
+			this.toolStripComboBox1.Name = "toolStripComboBox1";
+			this.toolStripComboBox1.Size = new System.Drawing.Size(197, 23);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(522, 279);
+			this.Controls.Add(this.panel1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainForm";
+			this.Text = "ej1";
+			this.ResizeEnd += new System.EventHandler(this.MainFormResizeEnd);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ P9/ej1/MainForm.cs
@@ -0,0 +1,209 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Carlos
+ * Date: 11/12/2017
+ * Time: 04:43 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ej1
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string dir;
+		string pic;
+		Queue cola;
+		Form pantallaCompleta;
+		System.Windows.Forms.PictureBox pictureBox2;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			toolStripComboBox1.Items.AddRange(new string[] {
+			"Intervalo de presentación: 2 seg.",
+			"Intervalo de presentación: 4 seg.",
+			"Intervalo de presentación: 6 seg.",
+			"Intervalo de presentación: 8 seg.",
+			"Intervalo de presentación: 10 seg."});
+			toolStripComboBox1.SelectedIndex = 0;
+			dir = Directory.GetCurrentDirectory();
+			foreach (FileInfo f in new DirectoryInfo(dir).GetFiles())
+			{
+				if(f.Extension.ToLower() == ".jpeg" || f.Extension.ToLower() == ".jpg" || f.Extension.ToLower() == ".gif" || f.Extension.ToLower() == ".ico" || f.Extension.ToLower() == ".bmp")
+				{
+					listBox1.Items.Add(f.Name);
+				}
+			}
+			toolStripStatusLabel1.Text = dir;
+			//mostar primera iamgen si la hay
+			if(listBox1.Items.Count > 0)
+			{
+				draw(listBox1.Items[0].ToString());
+			}
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void AbrirCarpetaToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) {
+				dir = folderBrowserDialog1.SelectedPath;
+				DirectoryInfo  d = new DirectoryInfo(dir);
+				listBox1.Items.Clear();
+				foreach (FileInfo f in d.GetFiles())
+				{
+					if(f.Extension.ToLower() == ".jpeg" || f.Extension.ToLower() == ".jpg" || f.Extension.ToLower() == ".gif" || f.Extension.ToLower() == ".ico" || f.Extension.ToLower() == ".bmp")
+					{
+						listBox1.Items.Add(f.Name);
+					}
+				}
+				toolStripStatusLabel1.Text = dir;
+				//mostar primera iamgen si la hay
+				if(listBox1.Items.Count > 0)
+				{
+					draw(listBox1.Items[0].ToString());
+				}
+				else
+				{
+					draw("none");
+				}
+			}
+		}
+		void SalirToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			//cerrar programa
+			this.Close();			
+		}
+		void AjustarALaVentanaToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			if(ajustarALaVentanaToolStripMenuItem.Checked)
+			{
+				ajustarALaVentanaToolStripMenuItem.Checked = false;
+			}
+			else
+			{
+				ajustarALaVentanaToolStripMenuItem.Checked = true;
+			}
+			draw(pic);
+		}
+		void AToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			if(aToolStripMenuItem.Checked)
+			{
+				splitContainer1.Panel1Collapsed = true;
+				aToolStripMenuItem.Checked = false;
+			}
+			else
+			{
+				splitContainer1.Panel1Collapsed = false;
+				aToolStripMenuItem.Checked = true;
+			}
+			draw(pic);
+		}
+		void PresentaciónToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			presentaciónToolStripMenuItem.Checked = true;
+			//encolar imagenes
+			cola = new Queue(listBox1.Items);
+			//pantalla completa
+			pantallaCompleta = new Form();
+			pantallaCompleta.FormBorderStyle = FormBorderStyle.None;
+			pantallaCompleta.WindowState = FormWindowState.Maximized;
+			//nuevo pictureBox
+			pictureBox2 = new System.Windows.Forms.PictureBox();
+			pictureBox2.Location = new System.Drawing.Point(0, 0);
+			pictureBox2.Name = "pictureBox2";
+			pictureBox2.TabIndex = 0;
+			pictureBox2.TabStop = false;
+			//mostrar
+			pantallaCompleta.Controls.Add(pictureBox2);
+			pantallaCompleta.Show();
+			draw((string)cola.Peek());
+			//evento ECS para cerrar pantalla Completa
+			pantallaCompleta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainFormKeyPress);
+			//iniciar timer
+			timer1.Interval = toolStripComboBox1.SelectedIndex * 2000 + 2000;
+			timer1.Enabled = true;
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			//cambiar a imagen siguiente
+			cola.Enqueue(cola.Dequeue());
+			draw((string)cola.Peek());
+			
+		}
+		void ListBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+			draw(listBox1.SelectedItem.ToString());
+		}
+		void MainFormResizeEnd(object sender, EventArgs e)
+		{
+			draw(pic);
+		}
+		void draw(string image)
+		{
+			pic = image;
+			if(presentaciónToolStripMenuItem.Checked)
+			{
+				pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+				pictureBox2.Size = pantallaCompleta.ClientSize;
+				if(pic != "none")
+				{
+					pictureBox2.Image = Image.FromFile(dir + "\\" + pic);
+				}
+				else
+				{
+					pictureBox2.Image = null;
+				}
+			}
+			else
+			{
+				if(ajustarALaVentanaToolStripMenuItem.Checked)
+				{
+					splitContainer1.Panel2.AutoScroll = false;
+					pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+					pictureBox1.Size = splitContainer1.Panel2.ClientSize;
+				}
+				else
+				{
+					splitContainer1.Panel2.AutoScroll = true;
+	
+					pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+				}
+			
+				if(pic != "none")
+				{
+					pictureBox1.Image = Image.FromFile(dir + "\\" + pic);
+				}
+				else
+				{
+					pictureBox1.Image = null;
+				}
+			}
+		}
+		void MainFormKeyPress(object sender, KeyPressEventArgs e)
+		{
+			if(e.KeyChar == (char)27)
+			{
+				timer1.Enabled = false;
+				presentaciónToolStripMenuItem.Checked = false;
+				pantallaCompleta.Close();
+				draw(pic);
+			}
+		}
+	}
+}
